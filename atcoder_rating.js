a735f11adb402c50ff75c590ced438e203a69969
koba-e964 <koba_max@kobas-Air.pohe>
2017-09-06 21:22:18 +0900

    Capture Enter key, create js file

---
+++ atcoder_rating.js
@@ -0,0 +1,44 @@
+function bigf(n) {
+	var num = 1.0;
+	var den = 1.0;
+	for (var i = 0; i < n; ++i) {
+		num *= 0.81;
+		den *= 0.9;
+	}
+	num = (1 - num) * 0.81 / 0.19;
+	den = (1 - den) * 0.9 / 0.1;
+	return Math.sqrt(num) / den;
+}
+
+function f(n) {
+	var finf = bigf(400);
+	return (bigf(n) - finf) / (bigf(1) - finf) * 1200.0;
+}
+
+function calc_rating(arr) {
+	var n = arr.length;
+	var num = 0.0;
+	var den = 0.0;
+	for (var i = n - 1; i >= 0; --i) {
+		num *= 0.9;
+		num += 0.9 * Math.pow(2, arr[i] / 800.0);
+		den *= 0.9;
+		den += 0.9;
+	}
+	var rating = Math.log2(num / den) * 800.0;
+	rating -= f(n);
+	return rating;
+}
+
+function calc_rating_from_last(last, perf, n) {
+	last += f(n);
+	var wei = 9 - 9 * 0.9 ** n;
+	var num = wei * (2 ** (last / 800.0)) + 2 ** (perf / 800.0) ;
+	var den = 1 + wei;
+	var rating = Math.log2(num / den) * 800.0;
+	rating -= f(n + 1);
+	return rating;
+}
+
+
+
